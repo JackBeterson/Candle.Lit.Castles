44909c6be4fd702c47de225d7461ccfece4c5b29
Jack Beterson <topyath@gmail.com>
2022-02-17 22:53:18 -0500

    Feb 17

--- Candle Lit Castle/Assets/Scripts/playerDash.cs
+++ Candle Lit Castle/Assets/Scripts/playerDash.cs
@@ -5,62 +5,69 @@ public class playerDash : MonoBehaviour
 {
     [SerializeField] private UnityEngine.Experimental.Rendering.Universal.Light2D candle;
     [SerializeField] private GameObject dashHitbox;
+    [SerializeField] private GameObject playerHurtbox;
     [SerializeField] private ParticleSystem dashTrail;
     [SerializeField] private Renderer render;
+    [SerializeField] private Animator animator;
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Color red;
     [SerializeField] private Color blue;
+
     private bool dashReady = true;
-    private bool hasTouchedGround = true;
-    private float dashpower = 5f;
+    public bool hasTouchedGround = true;
+    public float dashPower = 1f;
+    private float dashCoolCounnter;
 
     void Update()
     {
-        
+        if (dashCoolCounnter < 0f && hasTouchedGround)
+        {
+            dashReady = true;
+            candle.color = red;
+            render.material.SetColor("_Color", red);
+        }
+
+        if (dashCoolCounnter > 0f)
+        {
+            dashCoolCounnter -= Time.deltaTime;
+        }
     }
 
     public void Dash(InputAction.CallbackContext context)
     {
         if (context.performed && dashReady)
         {
-            rb.AddForce(new Vector2(Mathf.Sign(dashpower), 0f), ForceMode2D.Impulse);
+            rb.velocity = new Vector2(10f * dashPower, 0f);
 
             rb.constraints = RigidbodyConstraints2D.FreezeRotation | RigidbodyConstraints2D.FreezePositionY;
 
             GetComponent<playerMovement>().enabled = false;
+            dashHitbox.SetActive(true);
+            playerHurtbox.SetActive(false);
 
-            /*dashHitbox.SetActive(true);
+            animator.SetBool("Dash", true);
             hasTouchedGround = false;
-            dashing = true;
             dashReady = false;
-            dashCoolCounnter = 1.25f;
 
-            candle.color = blue;
             dashTrail.Play();
+            candle.color = blue;
             render.material.SetColor("_Color", blue);
-            
-            Invoke("DashReset", .25f);
 
-            if (isFacingRight)
-            {
-                horizontal = dashPower;
-            }
-            else if (!isFacingRight)
-            {
-                horizontal = -dashPower;
-            }*/
+            dashCoolCounnter = 1.25f;
+            Invoke("DashReset", .25f);
         }
     }
 
     private void DashReset()
     {
-        /*dashing = false;
-        horizontal = fixedH;
-        dashTrail.Stop();
-        dashHitbox.SetActive(false);
-        */
         rb.constraints = RigidbodyConstraints2D.FreezeRotation;
 
         GetComponent<playerMovement>().enabled = true;
+        dashHitbox.SetActive(false);
+        playerHurtbox.SetActive(true);
+
+        animator.SetBool("Dash", false);
+        
+        dashTrail.Stop();
     }
 }
--- Candle Lit Castle/Assets/Scripts/playerMovement.cs
+++ Candle Lit Castle/Assets/Scripts/playerMovement.cs
@@ -3,22 +3,16 @@ using UnityEngine.InputSystem;
 
 public class playerMovement : MonoBehaviour
 {
-
     [SerializeField] private Animator animator;
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundLayer;
 
-    private bool dashing = false;
     private bool isFacingRight = true;
-
 
     private float speed = 3.5f;
     private float jumpingPower = 7f;
-    private float dashCoolCounnter;
-    private float dashPower = 2.8f;
     private float horizontal;
-    private float fixedH;
 
     void Update()
     {
@@ -26,37 +20,11 @@ public class playerMovement : MonoBehaviour
 
         animator.SetFloat("HorizontalV", Mathf.Abs(horizontal));
         animator.SetFloat("VerticalV", rb.velocity.y);
-        animator.SetBool("Dash", dashing);
-
-        /*if (dashCoolCounnter < 0f && hasTouchedGround)
-        {
-            dashReady = true;
-        }
-
-        if (dashCoolCounnter > 0f)
-        {
-            dashCoolCounnter -= Time.deltaTime;
-        }
-        */
-        if (!isFacingRight && horizontal > 0f)
-        {
-            Flip();
-        }
-        else if (isFacingRight && horizontal < 0f)
-        {
-            Flip();
-        }
-        /*
-        if (dashReady == true)
-        {
-            candle.color = red;
-            render.material.SetColor("_Color", red);
-        }
 
         if (IsGrounded())
         {
-            hasTouchedGround = true;
-        }*/
+            GameObject.Find("Player").GetComponent<playerDash>().hasTouchedGround = true;
+        }
     }
 
     public void Jump(InputAction.CallbackContext context)
@@ -67,7 +35,7 @@ public class playerMovement : MonoBehaviour
         }
     }
 
-    private bool IsGrounded()
+    public bool IsGrounded()
     {
         return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
     }
@@ -78,15 +46,20 @@ public class playerMovement : MonoBehaviour
         Vector3 localScale = transform.localScale;
         localScale.x *= -1f;
         transform.localScale = localScale;
+        GameObject.Find("Player").GetComponent<playerDash>().dashPower = horizontal;
     }
 
     public void Move(InputAction.CallbackContext context)
     {
-        fixedH = context.ReadValue<Vector2>().x;
+        horizontal = context.ReadValue<Vector2>().x;
 
-        if (!dashing)
+        if (!isFacingRight && horizontal > 0f)
         {
-            horizontal = context.ReadValue<Vector2>().x;
+            Flip();
+        }
+        else if (isFacingRight && horizontal < 0f)
+        {
+            Flip();
         }
     }
 }
